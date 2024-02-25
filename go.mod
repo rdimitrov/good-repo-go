2e99938e2a3de94a7b8793a55784b71f0bd3dca6
Radoslav Dimitrov <radoslav@stacklok.com>
2024-02-26 01:19:50 +0200

    Generate UUID for each HTTP request
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- go.mod
+++ go.mod
@@ -1,7 +1,13 @@
 module github.com/stacklok/dummyrepo-go
 
-go 1.20
+go 1.21
 
-require github.com/go-kit/log v0.2.1
+require (
+	github.com/go-kit/log v0.2.1
+	github.com/satori/go.uuid v1.2.0
+)
 
-require github.com/go-logfmt/logfmt v0.5.1 // indirect
+require (
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-kit/log"
+	uuid "github.com/satori/go.uuid"
 	"html"
 	stdlog "log"
 	"net/http"
@@ -13,17 +14,20 @@ func main() {
 	// Hey handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		logMessage(fmt.Sprintf("Hey, %q", html.EscapeString(r.URL.Path)))
+		logMessage(fmt.Sprintf("request_id:, %s", uuid.NewV4()))
 	})
 
 	// Bye handler
 	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
 		logMessage(fmt.Sprintf("Bye, %q", html.EscapeString(r.URL.Path)))
+		logMessage(fmt.Sprintf("request_id:, %s", uuid.NewV4()))
 	})
 
 	// Listen and serve
 	stdlog.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// logMessage logs a message
 func logMessage(msg string) {
 	logger := log.NewLogfmtLogger(os.Stdout)
 	logger.Log(msg)
