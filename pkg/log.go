66946720484d2e4defe5383ae92d523d98ff821d
Radoslav Dimitrov <radoslav@stacklok.com>
2024-02-27 19:29:57 +0200

    Use the proper etcd and logging packages
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- pkg/log.go
+++ pkg/log.go
@@ -1,26 +1,26 @@
 package pkg
 
 import (
-	"github.com/go-kit/log"
+	"github.com/rs/zerolog/log"
 	"os"
 )
 
 // Trusty logging examples
 
-// NewLogMsg - Low-score example - logging - "github.com/go-kit/log"
-func NewLogMsg(msg string) {
-	// Create a new logger
-	logger := log.NewLogfmtLogger(os.Stdout)
-
-	// Log the message
-	err := logger.Log(msg)
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
-//// NewLogMsg - High-score example - logging - "github.com/rs/zerolog/log"
+//// NewLogMsg - Low-score example - logging - "github.com/go-kit/log"
 //func NewLogMsg(msg string) {
+//	// Create a new logger
+//	logger := log.NewLogfmtLogger(os.Stdout)
+//
 //	// Log the message
-//	log.Info().Msg(msg)
+//	err := logger.Log(msg)
+//	if err != nil {
+//		os.Exit(1)
+//	}
 //}
+
+// NewLogMsg - High-score example - logging - "github.com/rs/zerolog/log"
+func NewLogMsg(msg string) {
+	// Log the message
+	log.Info().Msg(msg)
+}
